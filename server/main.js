bf7e48267a16f4090c95a44ca7979aa1cc47ad2e
mohkamfer <mohammedkamalfergany@yahoo.com>
2019-04-27 16:38:01 +0200

    User Create + Read + Login

--- server/main.js
+++ server/main.js
@@ -1,5 +1,113 @@
-import { Meteor } from 'meteor/meteor';
+import {
+  Meteor
+} from 'meteor/meteor';
+import {
+  Exams
+} from '../lib/collection/collections.js';
+import connectRoute from 'connect-route';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+  let userCount = Accounts.users.find({}).fetch().length;
+  if (userCount < 1) {
+    console.log('No users yet!');
+  } else {
+    console.log('Found ' + userCount + ' profile' + (userCount > 1 ? 's' : ''));
+  }
+
+  let examCount = Exams.find({}).fetch().length;
+  if (examCount < 1) {
+    console.log('No exams yet!');
+  } else {
+    console.log('Found ' + examCount + ' exams');
+  }
+});
+
+WebApp.connectHandlers.use(connectRoute(function (router) {
+  router.get('/profile/:username', function (req, res, next) {
+    let user = Accounts.users.findOne({ username: req.params.username });
+    if (user) {
+      res.writeHead(200);
+      res.end(JSON.stringify(user));
+    } else {
+      res.writeHead(404);
+      res.end('No profile found with username ' + req.params.username);
+    }
+  });
+
+  router.post('/profile/', function (req, res, next) {
+    if (req.headers) {
+      let headers = req.headers;
+      if (!headers.profile_firstname ||
+        !headers.profile_lastname ||
+        !headers.profile_username ||
+        !headers.profile_email ||
+        !headers.profile_password ||
+        !headers.profile_day ||
+        !headers.profile_month ||
+        !headers.profile_year ||
+        !headers.profile_gender) {
+        res.writeHead(400);
+        res.end('Operation needs firstname, lastname, ' +
+        'username, email, password, day, month, year and gender.');
+        return;
+      }
+
+      Accounts.createUser({
+        firstname: headers.profile_firstname,
+        lastname: headers.profile_lastname,
+        username: headers.profile_username,
+        email: headers.profile_email,
+        password: headers.profile_password,
+        day: headers.profile_day,
+        month: headers.profile_month,
+        year: headers.profile_year,
+        gender: headers.profile_gender
+      });
+
+      res.writeHead(200);
+      res.end('1');
+    }
+  });
+
+  router.post('/login/', function (req, res, next) {
+    if (req.headers) {
+      let headers = req.headers;
+      if (!headers.profile_email ||
+        !headers.profile_password) {
+        res.writeHead(400);
+        res.end('Operation needs email + password');
+        return;
+      }
+
+      let user = Accounts.users.findOne({ 'emails.address': headers.profile_email });
+      if (user) {
+        let result = Accounts._checkPassword(user, headers.profile_password);
+        if (result.error) {
+          res.writeHead(500);
+          res.end('' + result.error);
+        } else {
+          res.writeHead(200);
+          res.end(result.userId);
+        }
+      } else {
+        res.writeHead(404);
+        res.end('User not found');
+        return;
+      }
+    }
+  });
+}));
+
+Accounts.onCreateUser((options, user) => {
+  user.firstname = options.firstname;
+  user.lastname = options.lastname;
+  user.username = options.username;
+  user.email = options.email;
+  user.password = options.password;
+  user.day = options.day;
+  user.month = options.month;
+  user.year = options.year;
+  user.gender = options.gender;
+  return user;
 });
