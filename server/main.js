2ee5dc81501874716f6fff9a5f761aa988e9fe75
mohkamfer <mohammedkamalfergany@yahoo.com>
2019-04-27 22:13:17 +0200

    Exams: Create & Read

--- server/main.js
+++ server/main.js
@@ -23,7 +23,7 @@ Meteor.startup(() => {
 });
 
 WebApp.connectHandlers.use(connectRoute(function (router) {
-  router.post('/profile/', function (req, res, next) {
+  router.post('/profile', function (req, res, next) {
     if (req.headers) {
       let headers = req.headers;
       if (!headers.profile_firstname ||
@@ -71,7 +71,7 @@ WebApp.connectHandlers.use(connectRoute(function (router) {
     }
   });
 
-  router.get('/students/', function (req, res, next) {
+  router.get('/students', function (req, res, next) {
     let students = Accounts.users.find({ role: 'student' }).fetch();
     if (students) {
       res.writeHead(200);
@@ -89,7 +89,7 @@ WebApp.connectHandlers.use(connectRoute(function (router) {
     }
   });
 
-  router.post('/login/', function (req, res, next) {
+  router.post('/login', function (req, res, next) {
     if (req.headers) {
       let headers = req.headers;
       if (!headers.profile_email ||
@@ -119,7 +119,7 @@ WebApp.connectHandlers.use(connectRoute(function (router) {
     }
   });
 
-  router.post('/logout/', function (req, res, next) {
+  router.post('/logout', function (req, res, next) {
     if (req.headers) {
       let headers = req.headers;
       if (!headers.profile_userid) {
@@ -141,7 +141,7 @@ WebApp.connectHandlers.use(connectRoute(function (router) {
     }
   });
 
-  router.get('/loggedin/', function (req, res, next) {
+  router.get('/loggedin', function (req, res, next) {
     if (req.headers) {
       let headers = req.headers;
       if (!headers.profile_userid) {
@@ -165,7 +165,103 @@ WebApp.connectHandlers.use(connectRoute(function (router) {
       }
     }
   });
+
+  router.post('/exam', function (req, res, next) {
+    if (req.headers) {
+      let headers = req.headers;
+      if (!headers.exam_title ||
+        !headers.exam_author ||
+        !headers.exam_questions) {
+        res.writeHead(400);
+        res.end('Operation needs exam_title, exam_author and exam_questions!');
+        return;
+      }
+
+      let questions = headers.exam_questions;
+      try {
+        questions = JSON.parse(questions);
+        if (typeof questions === 'object' && questions.constructor === Array) {
+          let examId = Exams.insert({
+            title: headers.exam_title,
+            author: headers.exam_author,
+            questions: questions
+          });
+  
+          res.writeHead(200);
+          res.end('' + examId);
+        } else {
+          res.writeHead(400);
+          res.end('exam_questions is not a valid JSON array.');
+        }
+      } catch (e) {
+        res.writeHead(400);
+        res.end('exam_questions is not valid JSON.');
+      }
+    }
+  });
+
+  router.get('/exams', function (req, res, next) {
+    let exams = Exams.find({}).fetch();
+    if (exams) {
+      res.writeHead(200);
+      res.end(JSON.stringify(exams.map(function(exam) {
+        return {
+          _id: exam._id,
+          title: exam.title,
+          author: exam.author
+        };
+      })));
+    } else {
+      res.writeHead(404);
+      res.end('No exams found');
+    }
+  });
+
+  router.get('/exam/:id', function (req, res, next) {
+    let exam = Exams.findOne({ _id: req.params.id });
+    if (exam) {
+      res.writeHead(200);
+      res.end(JSON.stringify(exam));
+    } else {
+      res.writeHead(404);
+      res.end('Exam not found');
+    }
+  });
 }));
+
+var examTemplate = {
+  questions: [
+    { // Question Object
+      type: '1', // Multiple Choice
+      question: 'Select Choice Below C.',
+      default: 3 // Choice C
+    }, {
+      type: '2', // True or False
+      question: 'C++ Is Fun?',
+      default: true
+    }
+  ],
+  results: [
+    { // Student Result Object
+      studentId: 'ABCD', // userId for student
+      firstname: 'Mdohas',
+      lastname: 'Nofal',
+      questions: [
+        {
+          type: '1',
+          question: 'Select Choice Below C.',
+          default: 3,
+          answer: 3 // Correct Answer
+        }, {
+          type: '2', // True or False
+          question: 'C++ Is Fun?',
+          default: true,
+          answer: false // Wrong Answer
+        }
+      ]
+    }
+  ]
+};
 
 Accounts.onCreateUser((options, user) => {
   user.firstname = options.firstname;
